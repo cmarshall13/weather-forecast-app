0b1d723cff52039979cdb023298ae0eb5350b542
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-08-27 12:31:51 -0400

    made function to fetch current weather and display temp/wind/uvi/humidity on the page

--- assets/js/current-forecast.js
+++ assets/js/current-forecast.js
@@ -12,8 +12,52 @@ function fetchCurrentForecast(lat,lon,city) {
                     cityWind = data.current.wind_speed;
                     cityIcon = data.current.weather[0].icon;
                     cityDescription = (data.current.weather[0].description).toUpperCase();
-                })
+
+                    //create icon image
+                    let iconImage = document.createElement('img');
+                    iconImage.className = "current-icon";
+                    iconImage.src = 'https://openweathermap.org/img/wn/' + cityIcon + '.png';
+
+                    //add styling
+                    temp.className = 'current-info';
+                    wind.className = 'current-info';
+                    humidity.className = 'current-info';
+                    uvi.className = 'current-info';
+
+                    //create a span for UVI number
+                    uviSpan = document.createElement('span');
+                    uviSpan.textContent = cityUvi;
+
+
+                    //display data on page
+                    cityInfo.textContent = `${city} (${currentDate})`;
+                    temp.textContent = `Temp: ${cityTemp}\u00BF`;
+                    wind.textContent = `Wind: ${cityWind} MPH`;
+                    humidity.textContent = `Humidity: ${cityHumidity}%`;
+
+                    if (cityUvi < 3) {
+                        uviSpan.classList.add('favorable');
+                    } else if (cityUvi > 5) {
+                        uviSpan.classList.add('severe');
+                    } else {
+                        uviSpan.classList.add('moderate');
+                    }
+                    uvi.textContent = `UV Index: `;
+
+                    cityInfo.append(iconImage);
+                    uvi.append(uviSpan);
+
+                    fetchCurrentForecast.append(temp,wind,humidity,uvi);
+
+                    getFiveDayForecast(data);
+                });
+            } else {
+                alert(city + 'is not a valid city.');
             }
-        }
-    )
+        })
+        .catch(function (err){
+            console.log('Something went wrong!');
+            return;
+        })
+    
 }
