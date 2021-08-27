71ea243e9e623f2db2b54afad2af25bb823d7af1
Corinne Marshall <corinnemarshall@Corinnes-iMac.local>
2021-08-27 11:35:04 -0400

    created functions in script.js to store cities in localStorage and pass through the fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,66 @@ let storedCities = JSON.parse(localStorage.getItem('storedCities')) ||[];
 const api_key = '1fd353df9a65fba742aece36f28e320a';
 
 
+//display previously searched cities
+function displayButtons() {
+    storedCities= JSON.parse(localStorage.getItem('storedCities')) || [];
+
+    for (i= storedCities.length - 1; i>=0; i--) {
+        var cityButton = document.createElement('button');
+        cityButton.classList = 'btn city-btn';
+        cityButton.textContent = storedCities[i];
+        newButtons.append(cityButton);
+    };
+};
+
+//create a new button from search
+function createButton(city) {
+    var newCityButton = document.createElement('button');
+    newCityButton.classList = 'btn city-btn';
+    newCityButton.textContent = city;
+    newButtons.append(newCityButton);
+
+    fetchCoordinates(city);
+    citySearchInput.value = '';
+}
+
+//save searched city to local storage and display
+function getCityInfo(event) {
+    event.preventDefault();
+
+    //clear current city info
+    cityInfo.textContent = '';
+    forecastContainer.innerText = '';
+    weatherCardsContainer.innerText = '';
+
+    var city = citySearchInut.value.trim();
+    if(!city) {
+        alert('Not a valid city, please try again!');
+        return;
+    }
+    //get localStorage array
+    storedCities = JSON.parse(localStorage.getItem('storedCities')) || [];
+    //save new city to localStorage
+    storedCities.push(city);
+    storedCities = localStorage.setItem('storeCities', JSON.stringify(storedCities));
+
+    createButton(city);
+};
+
+//get info from local storage to display already searched cities(btns)
+displayButtons();
+
+//get city info on btn click
+searchButton.addEventListener('click', getCityInfo);
+
+//search an already existing (previously searched) city
+$(document).on('click', '.city-btn', function(event) {
+    //clear current city from search
+    cityInfo.textContent = '';
+    forecastContainer.innerText = '';
+    weatherCardsContainer.innerText = '';
+    //display searched city's date and coordinates
+    var existingCity = event.target.textcontent;
+    cityInfo.textContent = `${existingCity} (${currentDate})`;
+    fetchCoordinates(existingCity);
+});
